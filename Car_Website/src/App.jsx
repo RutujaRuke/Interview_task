3bf967993d05746b484df6b725bf811fbd884483
Rutuja Ruke <rutuja.ruke2004@gmail.com>
2024-11-24 20:15:49 +0530

    image added

--- Car_Website/src/App.jsx
+++ Car_Website/src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Home from './Components/Home'
 
 function App() {
 
   return (
     <>
-    <div className="h-screen flex items-center justify-center bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">Tailwind is working!</h1>
-    </div>
+    <Home/>
       
     </>
   )
